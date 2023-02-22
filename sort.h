8876e190f5381f3ad63c06dd9c392e5aff52e942
elcodo254 <ellawachaga@gmail.com>
2023-02-21 16:25:36 -0800

    clear white spaces in sort.h file

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -23,7 +23,7 @@ void print_list(const listint_t *list);
 void swap(int *first, int *second);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_node(listint_t **head, listint_t *node1, listint_t *node2); 
+void swap_node(listint_t **head, listint_t *node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
